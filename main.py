54c3591a45ae6db733bad69b5ff567cac24aa218
Krzysztof Głowacz <krzysztofglowacz88@gmail.com>
2026-06-09 21:07:57 +0200

    try to resolve the issue with SparkFiles using urllib.request lib

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from pyspark import SparkFiles
+import os
+import urllib.request
+import tempfile
+from pathlib import Path
+
 from pyspark.sql import SparkSession
 
+
 DATA_URL: str = "https://www.football-data.co.uk/mmz4281/2526/E0.csv"
+IS_DATABRICKS: bool = "DATABRICKS_RUNTIME_VERSION" in os.environ
 
+builder = SparkSession.builder.appName("databricks-demo")
 
-# Ensure SparkSession exists
-spark = (
-    SparkSession.builder
-    .appName("databricks-demo")
-    .master("local[*]")  # All available cores when running locally
-    .getOrCreate()
-)
+if not IS_DATABRICKS:
+    builder = builder.master("local[*]")  # Use all available cores when run locally
+
+# Ensure the SparkSession exists
+spark: SparkSession = builder.getOrCreate()
 
 
 def run() -> None:
-    spark.sparkContext.addFile(DATA_URL)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        local_path = Path(tmpdir) / "E0.csv"
+        print(f"[Databricks] Fetching data to: {local_path.as_posix()}")
+        _ = urllib.request.urlretrieve(DATA_URL, local_path)
 
-    df = spark.read.csv(
-        path="file://" + SparkFiles.get("E0.csv"), 
-        header=True, 
-        inferSchema=True
-    )
+        df = spark.read.csv(
+            path=f"file:{local_path}", 
+            header=True, 
+            inferSchema=True,
+        )
 
-    df.show()
+    df.show(10)
 
 
 if __name__ == "__main__":

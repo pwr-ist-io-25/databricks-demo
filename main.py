70faac5c89d023708d0e5b1857e11622b1a5807d
Krzysztof Głowacz <krzysztofglowacz88@gmail.com>
2026-06-09 20:44:45 +0200

    implement very basic script that demonstrates data fetching with SparkFiles

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from databricks-demo!")
+from pyspark import SparkFiles
+from pyspark.sql import SparkSession
+
+DATA_URL: str = "https://www.football-data.co.uk/mmz4281/2526/E0.csv"
+
+
+# Ensure SparkSession exists
+spark = (
+    SparkSession.builder
+    .appName("databricks-demo")
+    .master("local[*]")  # All available cores when running locally
+    .getOrCreate()
+)
+
+
+def run() -> None:
+    spark.sparkContext.addFile(DATA_URL)
+
+    df = spark.read.csv(
+        path="file://" + SparkFiles.get("E0.csv"), 
+        header=True, 
+        inferSchema=True
+    )
+
+    df.show()
 
 
 if __name__ == "__main__":
-    main()
+    run()
